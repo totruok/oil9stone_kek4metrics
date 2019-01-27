41fa641dacb818cda9c6741f0c6499a00cbf8fc8
Grigorii Ivanov <itisgrisha@gmail.com>
2019-01-27 14:46:04 +0300

    add rang

---
+++ rang.py
@@ -0,0 +1,14 @@
+GENDER_LIST = ['M', 'F']
+AGE_LIST = ['(0, 2)', '(4, 6)', '(8, 12)', '(15, 20)', '(25, 32)', '(38, 43)', '(48, 53)', '(60, 100)']
+AGE2INDEX = {a: i for i, a in enumerate(AGE_LIST)}
+GENDER2INDEX = {'M': 0, 'F': 1}
+
+
+def sort_questions(age, gender, questions):
+    return sorted(
+        questions,
+        key=lambda x:
+            1 * abs(AGE2INDEX[x['age']] - AGE2INDEX[age])
+            + 3 * abs(GENDER2INDEX[x['gender']] - GENDER2INDEX[gender])
+    )
+
