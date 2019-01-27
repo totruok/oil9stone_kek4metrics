8e5452d954e1e4452494b92b67d44ea0b4fd4bc0
Lionel Miller <delonorm@gmail.com>
2019-01-27 14:35:58 +0300

    Reformat code

--- age_gender.py
+++ age_gender.py
@@ -3,10 +3,10 @@ import tempfile
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
-from rude_carnie.yolodetect import PersonDetectorYOLOTiny
 from rude_carnie.guess import AGE_LIST, GENDER_LIST, RESIZE_FINAL, classify_one_multi_crop
+from rude_carnie.model import inception_v3
 from rude_carnie.utils import ImageCoder
-from rude_carnie.model import get_checkpoint, inception_v3
+from rude_carnie.yolodetect import PersonDetectorYOLOTiny
 
 
 class AgeGenderDetector:
@@ -16,9 +16,7 @@ class AgeGenderDetector:
 
         checkpoint_paths = {'age': age_path, 'gender': gender_path}
         self.label_lists = {'age': AGE_LIST, 'gender': GENDER_LIST}
-        self.image_inputs = {}
         self.softmax_outputs = {}
-
 
         config = tf.ConfigProto(allow_soft_placement=True)
 
@@ -35,11 +33,13 @@ class AgeGenderDetector:
                 )
                 slim.assign_from_checkpoint_fn(
                     model_path=tf.train.latest_checkpoint(checkpoint_paths[model_name]),
-                    var_list={k.name[k.name.find('/')+1:k.name.find(':')]: k for k in tf.contrib.framework.get_variables_to_restore(include=[model_name])}
+                    var_list={
+                        k.name[k.name.find('/') + 1:k.name.find(':')]: k
+                        for k in tf.contrib.framework.get_variables_to_restore(include=[model_name])
+                    }
                 )(self.session)
 
         print("[KEKLOG] Models loaded")
-
 
     def run(self, image):
         face_files, rectangles = self.face_detect.run_img(image)
@@ -65,6 +65,7 @@ class AgeGenderDetector:
 if __name__ == '__main__':
     from pathlib import Path
     import skimage.io
+
     age_gender_detector = AgeGenderDetector(
         work_dir=tempfile.mkdtemp(prefix='age-gender-'),
         yolo_path=Path('../models/YOLO_tiny.ckpt').expanduser(),
